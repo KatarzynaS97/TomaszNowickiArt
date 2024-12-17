9e86d8f9419c298815147b026158125a327e2900
KatarzynaS97 <katarzyna33445@wp.pl>
2024-12-17 22:24:55 +0100

    Add Card Component

---
+++ components/Card/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <Card
+      :dt="props.dt"
+      :pt="props.pt"
+      :pt-options="props.ptOptions"
+      :unstyled="props.unstyled"
+  >
+    <template #header>
+      <slot name="header" />
+    </template>
+
+    <template #title>
+      <slot name="title" />
+    </template>
+
+    <template #content>
+      <slot name="content" />
+    </template>
+  </Card>
+</template>
+<script setup lang="ts">
+import { CardInterface } from 'types'
+
+const props = defineProps<CardInterface>()
+</script>
